5a0f331108fce54d8f2fe7d96be39047ffb9b668
peer <benzhemin@gmail.com>
2015-12-27 16:48:21 +0800

    swift improve

--- SnapKitDemo/AppDelegate.swift
+++ SnapKitDemo/AppDelegate.swift
@@ -17,23 +17,18 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
-        window!.backgroundColor = UIColor.whiteColor()
-        
-        //window!.rootViewController = UINavigationController(rootViewController: ViewController()!)
-        
-        window!.rootViewController = UINavigationController(rootViewController: WHEqualViewController()!)
+        if let window = window{
+            window.backgroundColor = UIColor.whiteColor()
+            
+            window.rootViewController = UINavigationController(rootViewController: ViewController()!)
+            
+            //window.rootViewController = UINavigationController(rootViewController: WHEqualViewController()!)
+            window.makeKeyAndVisible()
+        }
         
         return true
     }
 
-    func application(application: UIApplication, willChangeStatusBarFrame newStatusBarFrame: CGRect) {
-        for window in UIApplication.sharedApplication().windows {
-            if window.dynamicType.self.description().containsString("UITextEffectsWindow") {
-                window.removeConstraints(window.constraints)
-            }
-        }
-    }
-    
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
