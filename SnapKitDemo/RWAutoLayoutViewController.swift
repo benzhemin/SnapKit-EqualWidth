82fb440423591b63ae4f4df7be6747ed9ed7cede
peer <benzhemin@gmail.com>
2015-12-30 11:24:42 +0800

    添加 RW AutoLayout 示例

---
+++ SnapKitDemo/RWAutoLayoutViewController.swift
@@ -0,0 +1,106 @@
+//
+//  RWAutoLayoutViewController.swift
+//  SnapKitDemo
+//
+//  Created by bob on 12/29/15.
+//  Copyright © 2015 bob. All rights reserved.
+//
+
+import UIKit
+
+class RWAutoLayoutViewController: UIViewController {
+
+    let upperLeft : UIView
+    let upperRight : UIView
+    let bottom : UIView
+    
+    //构造了一个无参数构造函数
+    init?(_ coder: NSCoder? = nil){
+        
+        self.upperLeft = UIView()
+        self.upperRight = UIView()
+        self.bottom = UIView()
+        
+        if let aCoder=coder {
+            super.init(coder: aCoder)
+        }else {
+            super.init(nibName: nil, bundle: nil)
+        }
+    }
+    
+    required convenience init?(coder: NSCoder){
+        self.init(coder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = UIColor.whiteColor()
+        
+        self.navigationController?.navigationBar.hidden = true
+        
+        self.upperLeft.backgroundColor = UIColor.greenColor()
+        self.upperRight.backgroundColor = UIColor.yellowColor()
+        self.bottom.backgroundColor = UIColor.blueColor()
+        
+        self.view.addSubview(self.upperLeft)
+        self.view.addSubview(self.upperRight)
+        self.view.addSubview(self.bottom)
+        
+        self.updateViewConstraints()
+    }
+    
+    override func updateViewConstraints() {
+        
+        let spacing = 20
+        
+        self.upperLeft.snp_makeConstraints { (make) -> Void in
+            make.leading.equalTo(self.view).offset(spacing)
+            make.top.equalTo(self.view).offset(spacing)
+        }
+        
+        self.upperRight.snp_makeConstraints { (make) -> Void in
+            make.leading.equalTo(self.upperLeft.snp_trailing).offset(spacing)//.priorityHigh()
+            make.trailing.equalTo(self.view).offset(-spacing)
+            make.top.equalTo(self.upperLeft.snp_top)
+            make.width.equalTo(self.upperLeft)//.priority(750)
+        }
+        
+        self.bottom.snp_makeConstraints { (make) -> Void in
+            make.top.equalTo(self.upperLeft.snp_bottom).offset(spacing)
+            make.bottom.equalTo(self.view.snp_bottom).offset(-spacing)
+            make.leading.equalTo(self.view.snp_leading).offset(spacing)
+            make.trailing.equalTo(self.view.snp_trailing).offset(-spacing)
+        }
+        
+        
+        upperRight.snp_makeConstraints { (make) -> Void in
+            make.height.equalTo(self.upperLeft.snp_height)//.priority(750)
+        }
+        
+        bottom.snp_makeConstraints { (make) -> Void in
+            make.height.equalTo(self.upperRight.snp_height).dividedBy(2.0)//.priority(750)
+        }
+        
+        super.updateViewConstraints()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        print("upper right frame \(NSStringFromCGRect(self.upperRight.frame))")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
